5a380cc71c5f0e8a3f0a6c599a3a912aae4c7793
michailtam <mi.tamvak@gmail.com>
2021-09-04 20:09:21 +0300

    Map server pkg added

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/mikelap/GitRepo/home-service-robot/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/mikelap/GitRepo/home-service-robot/devel;/opt/ros/noetic'.split(';') if '/home/mikelap/GitRepo/home-service-robot/devel;/opt/ros/noetic' != '' else []
