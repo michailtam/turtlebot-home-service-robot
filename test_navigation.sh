91497952aba8eaf3c59bfc1582ee840221052950
michailtam <mi.tamvak@gmail.com>
2021-09-06 22:08:03 +0300

    Parameters tweaked to execute 2d nav goal in rviz (robot does not 100% proper navigation on the calculated path)

--- test_navigation.sh
+++ test_navigation.sh
@@ -5,6 +5,4 @@ xterm  -e  " roslaunch my_robot world.launch" &
 sleep 5
 xterm  -e  " roslaunch my_robot amcl.launch" &
 sleep 5
-#xterm  -e  " roslaunch my_robot mapping.launch" &
-#sleep 5
 xterm  -e  " rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
